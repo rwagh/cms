fb1254d717c1e45805eb08d71034ba456590d79c
Ravindra Wagh <rwagh@customtravelsolutions.com>
2020-09-19 16:42:33 +0530

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="It's my first Vue App"/>
+  </div>
+</template>
+
+<script>
+//import Vue from 'vue'
+//import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0px;
+  padding: 0px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
